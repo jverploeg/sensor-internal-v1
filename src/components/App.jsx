646ff29ceb686134465a8e01719668cdc40d2c33
jverploeg <verploegjack@gmail.com>
2021-06-22 11:22:40 -0600

    cleaned up app.jsx and removed unused code. input fields now clear on submission, but added data doesn't show up in table until show/hide is clicked once or twice... so still need to work on data rendering.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -11,34 +11,57 @@ import RowModelControlGrid from './dataGrid';
 
 
 const App = () => {
-    //declare state
-    const [sample, setSample] = useState([]);
-    const [inputs, setInputs] = useState({});
-    const [columns, setColumns] = useState([]);
-    const [rows, setRows] = useState([]);
-    const [options, setOptions] = useState(false);
-    // Call the toggle hook which returns, current value and the toggler function
-    const [isTextChanged, setIsTextChanged] = useToggle();
+    //DEFINE STATE//////////////////
+    const [sample, setSample] = useState([]); //data from database
+    const [inputs, setInputs] = useState({}); // inputs from submission fields
+    const [columns, setColumns] = useState([]); // column name for tables
+    const [rows, setRows] = useState([]); // formatted rows from data so dataGrid can be filled correctly 
+    // custom state/hooks
+    const [isTextChanged, setIsTextChanged] = useToggle(); //Call the toggle hook which returns, current value and the toggler function
 
-    const [editRowsModel, setEditRowsModel] = useState({});
 
-    const handleEditRowModelChange = useCallback((params) => {
-        console.log({params})
-      setEditRowsModel(params.model);
-      //setRows(params.model);
-      //console.log({editRowsModel})
-      console.log({params})
-      console.log({rows})
-    }, []);
+    //USEEFFECT AND PAGE RERENDERING?////////////
+    //TODO: fix useeffect logic below, get rid of repetitive calls
+    //get data on initial page load? and if inputs change?
+    useEffect(() => {
+        getData()
+    },[]);
+
+    //useEffect to get table columns on initial page load... change later to specific table_name
+    useEffect(() => {
+        // getColumns();
+        // getRows();
+        getData();
+    },[isTextChanged])
+    useEffect(() => {
+        getColumns();
+        getRows();
+    },[sample])
+
+
+
+    //header for cors requests
+    //const header = {'Content-Type', undefined}
+    // set host to ip rather than localhost --> running into cors issues. come back and fix later
+    const host = `http://192.168.1.118:3000`;
+
+    //define Set Functions
+    const getData = async() => {
+        try {
+            const response = await axios.get(`${host}/sample`);
+            //console.log({response})
+            setSample(response.data);
+        }
+        catch (error) {
+            console.log(error)
+        }
+    }
+
+
+
     const handleEditCellChangeCommitted = useCallback(
         ({ id, field, props }) => {
-            console.log({id, field, props})
-            console.log({rows})
-            // let old = [rows[id - 1]];
-            // //old = old.value;
-            // let col = old[0];//[field];
-            // let key = col[field];
-            //console.log({old, col, field, key}, col[field])
+            // pass the col_name, row_id, and new_value to the router. will Update accordingly
             axios.put(`${host}/sample`, {id, field, props})
             .then(response => {
               console.log('axios in app response',response);
@@ -65,18 +88,9 @@ const App = () => {
         //[rows],
         });
 
-    //get data on initial page load? and if inputs change?
-    useEffect(() => {
-        getData()
-    },[]);
 
-    //useEffect to get table columns on initial page load... change later to specific table_name
-    useEffect(() => {
-        getColumns();
-        getRows();
-        getData();
-        console.log({columns, rows})
-    },[isTextChanged])
+
+
     const getColumns = () => {
         let temp = [];
         //iterate through any object and get the key names
@@ -93,18 +107,9 @@ const App = () => {
                 }
                 format.field = item; 
                 format.headerName = item;
-                // format.field = JSON.parse(JSON.stringify(arrayKeys[i]));
-                // format.headerName = JSON.parse(JSON.stringify(arrayKeys[i]));
                 temp.push(format);
             }
-            // arrayKeys.forEach(item => {
-            //     //console.log()
-            //     format.field = item; 
-            //     format.headerName = item;
-            //     temp.push(format);
-            // })
         }
-        
         //set the column state now
         setColumns(temp);
     }
@@ -115,84 +120,41 @@ const App = () => {
     */
     ///////////////////////////////////////
     const getRows = () => {
-        //set up template
         // need to get size of columns...
         let length = columns.length;
-        //console.log('size', {length})
-        // iterate 0-length and set up col key definitions....
         let format = {};
         let rowsTemp = [];
-        // for(let i = 0; i < length; i++){
-        //     let numToString = (i+1).toString();
-        //     let temp = 'col' + numToString;
-        //     format[temp] = '';
-        // }
-        //console.log({format});
         //now map the sample array to a rows state
-        //console.log(sample.length)
         for(let i = 0; i < sample.length; i++) {
             //format = sample[i]; NOOOOOOOO, this is a shallow copy!!!!!!!
             format = JSON.parse(JSON.stringify(sample[i]));
-            //format.id = (i+1);
-            //console.log(sample[i], format)
             rowsTemp.push(format);
         }
         setRows(rowsTemp);
-        //console.log({rows})
-
-        // const rows1 = [
-        //     { id: 1, count: 1, val: 'World' },
-        //     { id: 2, count: 2, val: 'is Awesome' },
-        //     { id: 3, count: 3, val: 'is Amazing' },
-        // ];
-        // setRows(rows1);
     }
-    const rowEdit = (event) => {
-        console.log(event)
-    }
-
 
 
     // Universal input bar handler
-    const onChangeHandler = useCallback(
-        ({target:{name,value}}) => setInputs(state => ({ ...state, [name]:value }), [])
-    );
-
-    //header for cors requests
-    //const header = {'Content-Type', undefined}
-    // set host to ip rather than localhost --> running into cors issues. come back and fix later
-    const host = `http://192.168.1.118:3000`;
-
-    //define Set Functions
-    const getData = async() => {
-        try {
-            const response = await axios.get(`${host}/sample`);
-            console.log({response})
-            setSample(response.data);
-        }
-        catch (error) {
-            console.log(error)
-        }
-    }
+    // const onChangeHandler = useCallback(
+    //     ({target:{name,value}}) => setInputs(state => ({ ...state, [name]:value }), [])
+    // );
+    const handleChange = e => {
+        setInputs(prevState => ({ ...prevState, [e.target.name]: e.target.value }));
+    }    
 
 
-    //useEffect to render/rerender
-    // useEffect(() => {
-    //     getData();
-    // },[inputs, options])
-    
-    //sample axios call on initial render
+
+
 
 
     //event handlers
     const showData = () => {
         setIsTextChanged();
-        //getData();
     }
+
+    //TODO: this is hard coded for now...Make dynamic for any table later on
     const handleSubmit = () => {
-        console.log({inputs});
         let num = parseInt(inputs.field1);
-        console.log({num})
         let temp = {
             count: num,
             val: inputs.field2
@@ -200,18 +162,15 @@ const App = () => {
         axios.post(`${host}/sample`, temp)
           .then(response => {
             console.log(response);
+            //setInputs({})
           })
           .catch(error => {
             console.log(error);
           });
 
         //clear input fields
-        setInputs({})  
-    }
-    const changeData = (event) => {
-        console.log({event});
-        // let target = getElementById(event);
-        // console.log({target})
+        setInputs({});
+        //getData({});  
     }
 
     //DOM
@@ -223,7 +182,6 @@ const App = () => {
             <div className = "body">
                 <button onClick={showData}>{isTextChanged ? 'Hide Data' : 'Show Data'}</button>
             </div>
-            {/* <code>editRowsModel: {JSON.stringify(editRowsModel)}</code> */}
             <div className = "table">
                 {!!isTextChanged &&
                     <div className = "foot" style={{ height: 400, width: '100%' }}>
@@ -232,22 +190,16 @@ const App = () => {
                                 columns={columns}
                                 rows={rows}
                                 onEditCellChangeCommitted={handleEditCellChangeCommitted}
-                                //onChange={(e) => rowEdit(e)}
-                                // editRowsModel={editRowsModel}
-                                // onEditRowModelChange={handleEditRowModelChange}
                             />
                         }
                     </div>
                 }
             </div>
             <div className = "addData">
-                {/* <form onSubmit={handleSubmit}>
-                    <input key="field1" name="field1" onChange={({target}) => setInputs(state => ({...state,field1:target.value}))} value={inputs.field1}/>
-                    <input key="field2" name="field2" onChange={({target}) => setInputs(state => ({...state,field2:target.value}))} value={inputs.field2}/>
-                    <button type="submit">Submit</button>
-                </form> */}
-                <input key="field1" name="field1" onChange={onChangeHandler} value={inputs.field1}/>
-                <input key="field2" name="field2" onChange={onChangeHandler} value={inputs.field2}/>
+                {/* <input key="field1" name="field1" onChange={onChangeHandler} value={inputs.field1 || ''}/>
+                <input key="field2" name="field2" onChange={onChangeHandler} value={inputs.field2 || ''}/> */}
+                <input name="field1" value={inputs.field1 || ''} onChange={handleChange} />
+                <input name="field2" value={inputs.field2 || ''} onChange={handleChange} />
                 <button key="text1" type="submit" onClick={handleSubmit}>ADD DATA</button>
             </div>
 
