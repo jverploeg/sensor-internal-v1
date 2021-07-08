35c83ce7e24ef60c44ee4c495e27869784c4181d
jverploeg <verploegjack@gmail.com>
2021-07-08 13:05:01 -0600

    back to reverted version.  image load only works when in project repo and imported at the top of the file

--- src/components/pdfGenerator.jsx
+++ src/components/pdfGenerator.jsx
@@ -18,25 +18,30 @@ const generatePDF = (data) => {
     //https://developer.mozilla.org/en-US/docs/Web/HTML/Element/img
 
     let imagePath = `D:/DATA/Sensor/webApp/images/housing/S8-Model.png`;
-    console.log({imagePath})
+    let imagePath2 = `file:///D:/DATA/Sensor/webApp/images/housing/S8-Model.png`;
+    console.log({imagePath2})
     //initialize
     const doc = new jsPDF('p', 'in');
-    var img = new Image();
-    //The Image() constructor creates a new HTMLImageElement instance. 
-    console.log({img})
-    img.crossOrigin = ""
-    img.src = imagePath; //path.resolve(imagePath)
-    //img.src = S8; //path.resolve(imagePath)
-    img.onload = function() {
-        doc.addImage(img, 'png', 5, 5, 5.0, 2.25);
-        //(imageData, format x, y, width, height)
-        //imageData 	string | HTMLImageElement | HTMLCanvasElement | Uint8Array imageData as base64 encoded DataUrl or Image-HTMLElement or Canvas-HTMLElement
-    };
-    console.log({img})
+    // var img = new Image();
+    // //The Image() constructor creates a new HTMLImageElement instance. 
+    // console.log({img})
+    // img.crossOrigin = ""
+    // //img.src = imagePath; //path.resolve(imagePath)
+    // img.src = S8; //path.resolve(imagePath)
+    // img.onload = function() {
+    //     doc.addImage(img, 'png', 5, 5, 5.0, 2.25);
+    //     //(imageData, format x, y, width, height)
+    //     //imageData 	string | HTMLImageElement | HTMLCanvasElement | Uint8Array imageData as base64 encoded DataUrl or Image-HTMLElement or Canvas-HTMLElement
+    // };
     //const test = doc.getImageProperties(img);
     // console.log({test})
     // doc.addImage(imagePath, 'png', 5, 5, 5.0, 2.25);
-    //doc.addImage(S8, 'png', 0, 0, 5.0, 2.25);//, 5, 5, 720, 324);
+    // var img = new Image();
+    // img.src = '../images/S8-Model.png';
+    // doc.addImage(img, 'png', 0, 0, 5.0, 2.25);//, 5, 5, 720, 324);
+
+    //this works..........
+    doc.addImage(S8, 'png', 0, 0, 5.0, 2.25);
     doc.save("a4.pdf");
     //startY is margin-top
 
