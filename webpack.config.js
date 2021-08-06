5220d181721dc184322d740a02922f70b4ccfa53
jverploeg <verploegjack@gmail.com>
2021-08-06 15:55:47 -0600

    issues with packages and material-ui

--- webpack.config.js
+++ webpack.config.js
@@ -50,6 +50,9 @@ module.exports = {
                 exclude: /node_modules/,
                 use: {
                     loader: 'babel-loader',
+                    // options: {
+                    //     presets: ['@babel/preset-env', '@babel/preset-react'],
+                    // }
                 },
             },
           // sass loader
