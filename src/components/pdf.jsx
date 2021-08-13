34fbf4a65f3eb0c79394d2650a8979f20e1b8fd7
jverploeg <verploegjack@gmail.com>
2021-08-13 15:46:58 -0600

    cleaned up old code and reorganized function definitions to follow logical flow

--- src/components/pdf.jsx
+++ src/components/pdf.jsx
@@ -20,6 +20,7 @@ const host = `http://192.168.1.118:3000`;
 const PDF = (input) => {
     //destructure props!!!!!!!!!!!!!!!
     let temp = input.input;//temp for testing
+    //TODO: temp = temp.toUpperCase();
     //let sensor = input.input;
 
     //////////////////TESTING//////////////////////
@@ -68,27 +69,29 @@ const PDF = (input) => {
     const [sensorCode, setSensorCode] = useState('');
     const [sensorData, setSensorData] = useState({});
     const [customData, setCustomData] = useState({});//different format than catalog
+
     //breakdown of parts/components
     const [type, setType] = useState('');
     const [type_description, setTypeD] = useState('');
-    //const [type_description2, setTypeD2] = useState('');//for option 2 with custom
     const [housing, setHousing] = useState('');
     const [char, setChar] = useState('');
     const [connect, setConnect] = useState('');
     const [option, setOption] = useState('');
     const [rev, setRev] = useState('');
     const [description, setDescription] = useState('');
-    //custom special states
+
+    //special states for combo images
     const [connChart, setConnChart] = useState('');
     const [specChart, setSpecChart] = useState('');
     const [picture, setPicture] = useState('');
-    //const [description2, setDescription2] = useState('');//for option 2 with custom
+
     //images
-    const [fileNames, setFileNames] = useState({}); //for custom sensor image selection
     const [images, setImages] = useState({});
+
     //html
     const [bullets, setBullets] = useState([]);
     const [htmlRaw, setHtmlRaw] = useState({});
+
     //booleans for flipping
     const [valid, setValid] = useState(false); //true until proven false?
     //////////////////////////////////////////////////////////////////////////////////////
@@ -101,14 +104,16 @@ const PDF = (input) => {
 
         setType('');
         setTypeD('');
-        //setTypeD2('');
         setHousing('');
         setChar('');
         setConnect('');
         setOption('');
         setRev('');
         setDescription('');
-        //setDescription2('');
+
+        setConnChart('');
+        setSpecChart('');
+        setPicture('');
 
         setImages({});
 
@@ -128,8 +133,7 @@ const PDF = (input) => {
         let senstype = check.type(sensor); // 'catalog', 'custom', 'xproto'
         //set type
         setSensorType(senstype);
-        console.log(senstype, sensor);
-
+        //check if this is a valid sensor code
         let find = (senstype, sensor) => {
             check.valid(senstype, sensor)
             .then(result => {
@@ -147,24 +151,13 @@ const PDF = (input) => {
         find(senstype, sensor);
     },[sensor])
 
-    // //if valid state changes, continue with data retrieval
-    // useEffect(() => {
-    //     if(valid){
-    //         setSensorCode(sensor);//input string
-    //     } 
-    //     else{
-    //         setSensorCode('');//set code to empty string
-    //     }
-    // },[sensor,valid])
-
-    //once code is set, get data
+    //once code is set & confirmed valid, get data
     useEffect(() => {
         if(sensorCode.length > 1) {
             if (sensorType === 'catalog') {
                 getSensor(sensorCode);
             } else if(sensorType === 'custom') {
                 getSensor(sensorCode);
-                //getCustom(sensorCode);
             } else if(sensorType === 'xproto') {
                 //TODO LATER: implement logic for xproto sensors
                 //getProto
@@ -172,14 +165,15 @@ const PDF = (input) => {
         }
     },[sensorCode])
 
-    
+    //catalog
     useEffect(() => {
         if(sensorData.part_number) {
             let segments = sensorCode.split('-');
             getType(segments[1]);
         }
     },[sensorData])
-    //once we have custom data package, call special breakdown to split into relevant parts
+
+    //once we have custom data package, need type from char
     useEffect(() => {
         if(customData.part_number) {
             setChar(customData.char);
@@ -190,7 +184,7 @@ const PDF = (input) => {
     //once we have sensor data package, call breakdown to split into relevant parts
     useEffect(() => {
         if(sensorType === 'catalog' && type.length > 1) {
-            breakdown();//sensorData, sensorCode);
+            breakdown();
         }
         if(sensorType === 'custom' && type.length > 1) {
             customBreakdown();
@@ -209,7 +203,7 @@ const PDF = (input) => {
             }
         }
     },[description])//trigger on description change.(last state to be set in Breakdown)
-    //getCustomImages
+    
 
     /////////////////////////////////////////////////////////////////////////////////////////
 
@@ -226,19 +220,17 @@ const PDF = (input) => {
         } else if(sensorType === 'custom'){
             try {
                 const { data } = await axios.get(`${host}/custom/${sensor}`);
-                console.log({data})
                 setCustomData(data[0]);
             }
             catch (error) {
                 console.log(error)
             }
         }
-
     }
+
     const getType = async(type) => {
         if(sensorType === 'custom') {
             try {
-                console.log(type)
                 const { data } = await axios.get(`${host}/ctype/${type}`);
                 setType(data[0].type);
             }
@@ -257,85 +249,6 @@ const PDF = (input) => {
         }
 
     }
-    const getImages = async() => {
-        const responses = await Promise.allSettled([
-            axios.get(`${host}/images/type/Type-${type}-Model`, { responseType: 'arraybuffer' }),
-            axios.get(`${host}/images/mech/${housing}-Mech-Model`, { responseType: 'arraybuffer' }),
-            axios.get(`${host}/images/housing/${housing}-Model`, { responseType: 'arraybuffer' }),
-            axios.get(`${host}/images/option/${option}-Model`, { responseType: 'arraybuffer' }),
-            axios.get(`${host}/images/connect/${connect}-Model`, { responseType: 'arraybuffer' }),
-            axios.get(`${host}/images/conn_charts/${connChart}-Model`, { responseType: 'arraybuffer' }),
-            axios.get(`${host}/images/spec_charts/${specChart}-Model`, { responseType: 'arraybuffer' }),
-            axios.get(`${host}/images/pictures/${picture}-Model`, { responseType: 'arraybuffer' }),
-            // axios.get(`${host}/images/conn_charts/${connect}-${char}-Model`, { responseType: 'arraybuffer' }),
-            // axios.get(`${host}/images/spec_charts/${char}-${option}-Model`, { responseType: 'arraybuffer' }),
-            // axios.get(`${host}/images/pictures/${housing}-${char}-Model`, { responseType: 'arraybuffer' }),
-        ])
-        let results = [];
-        for(let i = 0; i < responses.length; i++) {
-            if(responses[i].status === 'fulfilled'){
-                results.push(responses[i].value.data)//value is an object
-            } else {
-                results.push(null)
-            }
-        }
-        let images = convert.images(results);
-        setImages(images);
-    }
-    // const getCustomImages = async() => {
-    //     //call all the waterfalls to get an image for each section
-    //     const response = await Promise.all([
-    //         calls.getCustomImageType(sensorCode, type),
-    //         calls.getCustomImageMech(sensorCode, housing),
-    //         calls.getCustomImageHousing(sensorCode, housing),
-    //         calls.getCustomImageOption(sensorCode, option),
-    //         calls.getCustomImageConnect(sensorCode, connect),
-    //         calls.getCustomImageConnChart(sensorCode, connect, char),
-    //         calls.getCustomImageSpec(sensorCode, char, option),
-    //         calls.getCustomImagePicture(sensorCode, housing, char, type),
-    //     ]);
-    //     const data = response.map((response) => response.data);
-    //     let images = convert.images(data);
-    //     setImages(images);
-    // }
-    const getCustomImages = async() => {
-        console.log({customData})
-        //call all the waterfalls to get an image for each section
-        // const responses = await Promise.allSettled([
-        //     calls.getCustomImageType(sensorCode, type),
-        //     calls.getCustomImageMech(sensorCode, housing),
-        //     calls.getCustomImageHousing(sensorCode, housing),
-        //     calls.getCustomImageOption(sensorCode, option),
-        //     calls.getCustomImageConnect(sensorCode, connect),
-        //     calls.getCustomImageConnChart(sensorCode, connect, char),
-        //     calls.getCustomImageSpec(sensorCode, char, option),
-        //     calls.getCustomImagePicture(sensorCode, housing, char, type),
-        // ]);
-        const responses = await Promise.allSettled([
-            axios.get(`${host}/images/type/Type-${type}-Model`, { responseType: 'arraybuffer' }),
-            axios.get(`${host}/images/mech/${housing}-Mech-Model`, { responseType: 'arraybuffer' }),
-            axios.get(`${host}/images/housing/${housing}-Model`, { responseType: 'arraybuffer' }),
-            axios.get(`${host}/images/option/${option}-Model`, { responseType: 'arraybuffer' }),
-            axios.get(`${host}/images/connect/${connect}-Model`, { responseType: 'arraybuffer' }),
-            axios.get(`${host}/images/conn_charts/${connChart}-Model`, { responseType: 'arraybuffer' }),
-            axios.get(`${host}/images/spec_charts/${specChart}-Model`, { responseType: 'arraybuffer' }),
-            axios.get(`${host}/images/pictures/${picture}-Model`, { responseType: 'arraybuffer' }),
-        ])
-        console.log(responses)//different than catalog. all fulfilled, some vals are undefined....
-        let results = [];
-        for(let i = 0; i < responses.length; i++) {
-            if(responses[i].value){
-                results.push(responses[i].value.data)//value is an object
-            } else {
-                results.push(null)
-            }
-        }
-        console.log(results)
-        let images = convert.images(results);
-        setImages(images);
-    }
-    /////////////////////////////////////////////
-
 
     /////////////DATA FORMATTING/////////////////
     const breakdown = () => {
@@ -343,18 +256,19 @@ const PDF = (input) => {
         let segments = sensorCode.split('-');
         let H = segments[0];
         let C = segments[1];
-        setHousing(H);
-        setChar(C);
         let sensor_code = sensorData.sensor_code;
         let splitOps = sensor_code.split(H); 
         let Conn = splitOps[1];
-        setConnect(Conn);
         let opt = splitOps[0].slice(C.length); //get accurate option code
+
+        setHousing(H);
+        setChar(C);
+        setConnect(Conn);
         setOption(opt);
         setRev(sensorData.rev)
         setDescription(sensorData.title)
-
-        setConnChart(Conn + '-' + C);//P21-EHS1-Model
+        //combo image file format
+        setConnChart(Conn + '-' + C);
         setSpecChart(C + '-' + opt);
         setPicture(H + '-' + C);
 
@@ -368,34 +282,27 @@ const PDF = (input) => {
 
     const customBreakdown = (sensor) => {
         //destructure redefine data/props
-        setTypeD('');
         let housing = customData.housing;
-        setHousing(housing);
         let conn = customData.connection;
-        setConnect(conn);
         let opt = customData.option;
+
+        //set states
+        setTypeD('');
+        setHousing(housing);
+        setConnect(conn);
         setOption(opt);
         setRev(customData.rev);
         setDescription(customData.title);
-
         //special custom states for inconsistent file-naming
         setConnChart(customData.conn_chart);
         setSpecChart(customData.spec_chart);
         setPicture(customData.picture);
 
-        //option2 for highlighting catalog code after csxxxx
-        //DAVE DIDN"T CARE FOR THIS
-        // let temp = customData.title;
-        // temp = temp.split(', ');
-        // var type_description2 = temp[0];
-        // temp.unshift();
-        // var description2 = temp.join(', ');
-        // setTypeD2(type_description2);
-        // setDescription2(description2);
-
-        //getCustomHtml
+        //getHTML
         getCustomHtml(char, sensorCode);
     }
+
+    /////////////////////HTML/////////////////////////
     const getCustomHtml = async(char, sensor) => {
         try {
             const response = await Promise.all([
@@ -413,6 +320,52 @@ const PDF = (input) => {
     }
     /////////////////////////////////////////////
 
+    //////////////////////////////////////////IMAGES//////////////////////////////////////////////////////////
+    const getImages = async() => {
+        const responses = await Promise.allSettled([
+            axios.get(`${host}/images/type/Type-${type}-Model`, { responseType: 'arraybuffer' }),
+            axios.get(`${host}/images/mech/${housing}-Mech-Model`, { responseType: 'arraybuffer' }),
+            axios.get(`${host}/images/housing/${housing}-Model`, { responseType: 'arraybuffer' }),
+            axios.get(`${host}/images/option/${option}-Model`, { responseType: 'arraybuffer' }),
+            axios.get(`${host}/images/connect/${connect}-Model`, { responseType: 'arraybuffer' }),
+            axios.get(`${host}/images/conn_charts/${connChart}-Model`, { responseType: 'arraybuffer' }),
+            axios.get(`${host}/images/spec_charts/${specChart}-Model`, { responseType: 'arraybuffer' }),
+            axios.get(`${host}/images/pictures/${picture}-Model`, { responseType: 'arraybuffer' }),
+        ])
+        let results = [];
+        for(let i = 0; i < responses.length; i++) {
+            if(responses[i].status === 'fulfilled'){
+                results.push(responses[i].value.data)
+            } else {
+                results.push(null)
+            }
+        }
+        let images = convert.images(results);
+        setImages(images);
+    }
+    const getCustomImages = async() => {
+        const responses = await Promise.allSettled([
+            axios.get(`${host}/images/type/Type-${type}-Model`, { responseType: 'arraybuffer' }),
+            axios.get(`${host}/images/mech/${housing}-Mech-Model`, { responseType: 'arraybuffer' }),
+            axios.get(`${host}/images/housing/${housing}-Model`, { responseType: 'arraybuffer' }),
+            axios.get(`${host}/images/option/${option}-Model`, { responseType: 'arraybuffer' }),
+            axios.get(`${host}/images/connect/${connect}-Model`, { responseType: 'arraybuffer' }),
+            axios.get(`${host}/images/conn_charts/${connChart}-Model`, { responseType: 'arraybuffer' }),
+            axios.get(`${host}/images/spec_charts/${specChart}-Model`, { responseType: 'arraybuffer' }),
+            axios.get(`${host}/images/pictures/${picture}-Model`, { responseType: 'arraybuffer' }),
+        ])
+        let results = [];
+        for(let i = 0; i < responses.length; i++) {
+            if(responses[i].value){
+                results.push(responses[i].value.data)//value is an object
+            } else {
+                results.push(null)
+            }
+        }
+        let images = convert.images(results);
+        setImages(images);
+    }
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
 
